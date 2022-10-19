b460a34a4e6e51be1ea6f0651266b0fadab02e76
Robbie Hammond <robbiehammond3@gmail.com>
2022-10-19 17:42:14 -0400

    add updated svm search

--- svm_search.py
+++ svm_search.py
@@ -1,10 +1,11 @@
 import numpy as np
 from sklearn import svm
+from sklearn.feature_selection import SequentialFeatureSelector
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import roc_auc_score
 
-kernels = ['linear', 'poly', 'rbf', 'sigmoid']
-c_range = range(-5, 15 + 1, 1) # 2^-5 -> 2^3
+kernels = ['sigmoid', 'rbf', 'poly', 'linear']
+c_range = range(-5, 15 + 1, 1) # 2^-5 -> 2^15
 gamma_range = range(-15, 3 + 1, 1) #2^-15 -> 2^3
 features_to_use = [3, 4, 7, 8, 10, 19, 27, 28, 29]
 
@@ -20,12 +21,22 @@ max_score = 0
 
 def train_SVM_model(kernel, gamma, C):
     global max_score
+
+    # perform sbs
+    sfs = SequentialFeatureSelector(svm.SVC(kernel=kernel, gamma=gamma, C=C), n_features_to_select=27, scoring='roc_auc')
+    sfs.fit(X_train, y_train)
+    features = sfs.get_support()
+
     clf = svm.SVC(kernel=kernel, gamma=gamma, C=C)
-    clf.fit(X_train, y_train)
-    score = roc_auc_score(y_test, clf.predict(X_test))
+    new_X_train = X_train[:, features]
+    new_X_test = X_test[:, features]
+    clf.fit(new_X_train, y_train)
+    score = roc_auc_score(y_test, clf.predict(new_X_test))
+
+    print(score)
     if score > max_score:
         max_score = score
-        print("kernel: {}, gamma: {}, C: {}, score: {}".format(kernel, gamma, C, score))
+        print("kernel: {}, gamma: {}, C: {}, score: {}, features selected: {}".format(kernel, gamma, C, score, [i for i, x in enumerate(features) if x]))
 
 
 
